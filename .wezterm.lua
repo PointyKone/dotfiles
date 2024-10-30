d28b38a937a00737b1f6a3517784b37addf776b9
Kian <ksaeidnia06@gmail.com>
2024-10-30 20:43:02 +1000

    Add wizterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.default_prog = { 'powershell.exe', '-NoLogo' }
+
+config.color_scheme = 'Catppuccin Macchiato (Gogh)'
+config.window_background_opacity = 0.8
+
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 12
+config.text_background_opacity = 0.8
+
+config.window_decorations = 'NONE | RESIZE'
+
+return config
