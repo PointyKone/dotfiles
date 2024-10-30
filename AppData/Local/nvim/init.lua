04d27a8bad88d15a4c2cf52df1344ad91bc72bab
Kian <ksaeidnia06@gmail.com>
2024-10-30 20:44:12 +1000

    Add nvim config

---
+++ AppData/Local/nvim/init.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = ","
+require("config.lazy")
+
+vim.wo.number = true
+
+vim.keymap.set('n', '<C-f>', '<cmd>NvimTreeFocus<CR>')
+vim.keymap.set('n', '<C-t>', '<cmd>NvimTreeToggle<CR>')
+
+vim.keymap.set('n', 'ff', '<cmd>Telescope find_files<CR>', { desc = 'Telescope find files' })
+
+vim.cmd.colorscheme "catppuccin-macchiato"
+
+-- LSP --
+require("lsp.init")
+
+-- Lauline --
+require('lualine').setup({
+	options = {
+		theme = "nightfly"
+	}
+})
---
+++ AppData/Local/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+	{ 'VonHeikemen/lsp-zero.nvim', branch = 'v4.x' },
+	{ 'neovim/nvim-lspconfig' },
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/nvim-cmp' },
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	{ "nvim-lua/lsp-status.nvim" },
+	{ "folke/lsp-colors.nvim" }
+}
