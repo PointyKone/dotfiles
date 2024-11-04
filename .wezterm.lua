e4164402f7dae5355e868a0f194c4c9d6d6305fb
Kian Saeidnia <ksaeidnia06@gmail.com>
2024-11-05 00:55:50 +1000

    updated wezterm config and Lazy plugins

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,4 +12,10 @@ config.text_background_opacity = 0.9
 
 config.window_decorations = 'NONE | RESIZE'
 
+config.animation_fps = 1
+
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.cursor_blink_rate = 0
+
 return config
